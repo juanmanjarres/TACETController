1b6226c73a26a476d6b1a057c94103cd8c4c8b0d
Juan Manjarres <juacaman@gmail.com>
2020-01-19 14:53:42 -0600

    Created a basic interface for the app, with a switch and some textViews

--- app/src/main/java/com/saskinvent/tacetcontroller/MainActivity.java
+++ app/src/main/java/com/saskinvent/tacetcontroller/MainActivity.java
@@ -3,6 +3,7 @@ package com.saskinvent.tacetcontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initiate the switch
+        Switch power_switch = (Switch) findViewById(R.id.power_switch);
+
+        // check state of the switch
+        boolean state_powerswitch = power_switch.isChecked();
+
+        // if the powerswitch is on, the machine starts
+        if (state_powerswitch) {
+            // code to start the machine
+            System.out.println("Machine is on");
+        }
+
+
     }
 }
