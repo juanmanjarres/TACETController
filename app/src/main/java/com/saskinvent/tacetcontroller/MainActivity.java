283ebc3a246f07bb8b36c552d604db50a5afe8e6
Juan Manjarres <juacaman@gmail.com>
2020-01-26 14:43:44 -0600

    Imported bluetooth libs and made a basic function for it

--- app/src/main/java/com/saskinvent/tacetcontroller/MainActivity.java
+++ app/src/main/java/com/saskinvent/tacetcontroller/MainActivity.java
@@ -2,8 +2,15 @@ package com.saskinvent.tacetcontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Switch;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +26,42 @@ public class MainActivity extends AppCompatActivity {
         boolean state_powerswitch = power_switch.isChecked();
 
         // if the powerswitch is on, the machine starts
+        // this does not work if the switch is turned off, maybe set an onClickListener?
         if (state_powerswitch) {
             // code to start the machine
+
+
+            // int bluetoothWorks = useBluetooth(); this might not be the best way, maybe
+            // change return type of the function
+
+
             System.out.println("Machine is on");
         }
+
+        Button openfile_btn = (Button) findViewById(R.id.openfile_btn);
+
+        //openfile_btn.setOnClickListener(new OnClickListener() {
+
+        //});
+    }
+
+
+    protected int useBluetooth(){
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null){
+            // Device does not support bluetooth
+            return -1;
+        }
+        else {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBTIntent, 0);
+            }
+            Set<BluetoothDevice> pairedDevices;
+            pairedDevices = bluetoothAdapter.getBondedDevices();
+            // here implement to access the bluetooth of the machine
+            return 0;
+        }
+
     }
 }
